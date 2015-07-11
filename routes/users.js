1113d396fc0976e3ba9f0234098fa229f191ddb8
Alan <richard.alan.oli@gmail.com>
2015-07-11 03:48:37 -0500

    Agregué prueba de sessiones con express

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,22 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function(req, res) {
+  res.send(req.session.user);
+});
+
+router.get("/createSession", function(req, res) {
+  req.session.user = {
+    user_id : "user_id1",
+    user_name : "Alan"
+  };
+  res.send(req.session.user);
+});
+
+router.get("/destroy", function(req, res) {
+  req.session.destroy(function (){
+    res.send("session destruida");
+  });
 });
 
 module.exports = router;
