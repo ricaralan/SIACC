4a70ae8af7973e6b87d341523aeeb9efa1f3f68a
Alan <richard.alan.oli@gmail.com>
2015-09-14 22:42:39 -0500

    I apply permissions on "areas" view

--- routes/areas.js
+++ routes/areas.js
@@ -1,9 +1,22 @@
 var express = require("express");
 var router = express.Router();
 var areasController = require("../database/controllers/AreasController");
+var permisoController = require("../database/controllers/PermisosController");
 
 router.get("/", function(req, res) {
-  res.render('areas', { title: 'SIACC'});
+  if(req.user) {
+    permisoController.getPermisoTipoUsuario(req.user.usu_id_tipo_usuario,
+      "areas", function(err, permiso) {
+        if(!err && permiso && permiso[0].moa_ver==1) {
+          res.render('areas', { title: 'SIACC'});
+        } else {
+          console.log(err);
+          res.render("sin_permiso_vista", {title:"No tienes permisos para ver esto - SIACC"});
+        }
+    });
+  } else {
+    res.render("login", {title:"login"});
+  }
 });
 
 router.get("/getAreas", function(req, res) {
