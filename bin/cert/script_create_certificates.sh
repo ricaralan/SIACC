261ac6bd45befa39e214f1e081d00fdc684fcd38
Alan <richard.alan.oli@gmail.com>
2015-09-27 04:01:28 -0500

    fix

--- bin/cert/script_create_certificates.sh
+++ bin/cert/script_create_certificates.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 openssl genrsa -out key.pem 2048
 openssl req -sha256 -new -key key.pem -out key-csr.pem
 openssl x509 -req -sha256 -in key-csr.pem -signkey key.pem -out cert.pem -days 365
