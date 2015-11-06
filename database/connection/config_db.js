65800bc342d4b6e5c22009011544fe640f74178b
Alan <richard.alan.oli@gmail.com>
2015-11-06 13:09:56 -0600

    I added photo centro_de_computo.jpg of types of areas

--- database/connection/config_db.js
+++ database/connection/config_db.js
@@ -4,7 +4,7 @@
 module.exports = {
     mysql : {
       host     : "localhost",
-      database : "db_siacc2",
+      database : "db_siacc",
       user     : "root",
       password : "RAOR940203"
     }/*, sqlserver : {
